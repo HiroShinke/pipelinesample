9eb22cdf5dc30a2e78a05022031ef6a216cedf64
新家博文 <hiro@molloy.local>
2021-04-24 17:15:50 +0900

    use junit annotation

--- src/test/java/com/sample/AppTest.java
+++ src/test/java/com/sample/AppTest.java
@@ -1,39 +1,19 @@
-package com.sample;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+package com.sample;
 
 import static org.hamcrest.core.Is.*;
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp()
     {
 
