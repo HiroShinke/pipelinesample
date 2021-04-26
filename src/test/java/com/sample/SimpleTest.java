17f9cd7144ab8008e10822f41d995dd4908b0c1e
新家博文 <hiro@molloy.local>
2021-04-27 00:20:37 +0900

    use Cotegory

---
+++ src/test/java/com/sample/SimpleTest.java
@@ -0,0 +1,18 @@
+
+
+package com.sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import org.junit.experimental.categories.Categories;
+import org.junit.experimental.categories.Category;
+
+
+@RunWith(Categories.class)
+@Categories.IncludeCategory(Simple.class)
+@Suite.SuiteClasses(AppTest.class)
+    public class SimpleTest  {
+
+    }
